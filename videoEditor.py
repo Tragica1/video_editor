acd23a5e685221cfd5967fffca274812b2ce22d1
Maxim Odinokov <odi.max2002@gmail.com>
2022-09-25 01:04:26 +0300

    add video editor code

---
+++ videoEditor.py
@@ -0,0 +1,37 @@
+from moviepy.editor import VideoFileClip, concatenate_videoclips, TextClip, CompositeVideoClip
+
+
+class VideoEdit:
+    def __init__(self, video: VideoFileClip):
+        self.video = video
+        self.duration = video.duration
+
+    def add_text(self, text: str, fontsize: int, color: str, position: tuple, duration: int, start: int,
+                 end: int):
+        text_clip = (
+            TextClip(text, fontsize=fontsize, color=color)
+                .set_position(position)
+                .set_duration(duration)
+        )
+        first_part = self.video.subclip(0, start)
+        second_part = self.video.subclip(end, self.duration)
+        required_part = self.video.subclip(start, end)
+        video_with_text = CompositeVideoClip([required_part, text_clip])
+        result = concatenate_videoclips([first_part, video_with_text, second_part])
+        self.video = result
+
+    def change_resolution(self, type_of_res: int):
+        if type_of_res == 1:
+            result = self.video.resize(height=1920, width=1080)
+        elif type_of_res == 2:
+            result = self.video.resize(height=1080, width=1920)
+        else:
+            result = self.video.resize(height=1080, width=1080)
+        self.video = result
+
+    def trim_video(self, start: int, end: int):
+        first_part = self.video.subclip(0, start)
+        second_part = self.video.subclip(end, self.duration)
+        result = concatenate_videoclips([first_part, second_part])
+        self.video = result
+
