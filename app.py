7ec0ad7a64f3d4d0249a10cb1f06ce125b67b146
Maxim Odinokov <odi.max2002@gmail.com>
2022-09-25 01:03:44 +0300

    hotfix

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, flash, url_for
+from werkzeug.utils import secure_filename
+import os
+import videoEditor
 
 app = Flask(__name__, static_folder='static')
-app.config['UPLOAD_FOLDER'] = 'media'
+app.config['UPLOAD_FOLDER'] = 'static/media'
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-
+    if request.method == 'POST':
+        file = request.files['video_file']
+        if not file.filename:
+            flash('Видео не загружено!')
+        else:
+            filename = secure_filename(file.filename)
+            file.save((os.path.join(app.config['UPLOAD_FOLDER'], filename)))
+            fileLink = str((os.path.join(app.config['UPLOAD_FOLDER'], filename)))
     return render_template('index.html')
 
 
