76a7b92fd20d6c41980ea24c3bd9a854b1bf3847
Maxim Odinokov <odi.max2002@gmail.com>
2022-09-25 13:17:41 +0300

    final

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, send_from_directory
-from flask import render_template, request, flash, url_for
+from flask import render_template, request, flash, url_for, json
 from werkzeug.utils import secure_filename
 import os
-import videoEditor
+from videoEditor import VideoEdit
+import glob, time
 
 app = Flask(__name__, static_folder='static')
-app.config['UPLOAD_FOLDER'] = 'static/media'
+app.config['UPLOAD_FOLDER'] = 'static\\media'
+
+FRAME_RATE = 30
+cur_dir = os.getcwd()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,18 +19,29 @@ def index():
         if not file.filename:
             flash('Видео не загружено!')
         else:
-            filename = secure_filename(file.filename)
-            file.save((os.path.join(app.config['UPLOAD_FOLDER'], filename)))
-            fileLink = str((os.path.join(app.config['UPLOAD_FOLDER'], filename)))
+            fileName = secure_filename(file.filename)
+            # file.save((os.path.join(app.config['UPLOAD_FOLDER'], fileName)))
+            file.save((os.path.join('./', fileName)))
     return render_template('index.html')
 
 
 @app.route('/render', methods=['POST'])
-def index1():
+def render():
     if request.method == 'POST':
         cutJson = request.json
-        data = json.load(cutJson)
-    return render_template('index.html')
+        data = cutJson['framesToCut']
+        fileName = r'*.mp4'
+        file = glob.glob(fileName)[0]
+        editor = VideoEdit(file)
+        time1, time2 = 0, 0
+        for item in data:
+            time1, time2 = item[0] // FRAME_RATE, item[1] // FRAME_RATE
+        editor.trim_video(time1, time2)
+        editor.render()
+        time.sleep(10)
+        # os.remove((os.path.join(app.config['UPLOAD_FOLDER'], file)))
+    return render_template('return.html', ret='../output.mp4')
+
 
 
 if __name__ == '__main__':
